eb77e45c0005d2b88c25e48be2bea506490bd4cc
Chris <prepchrislee@gmail.com>
2022-02-22 20:52:42 -0500

    Added various sounds when player does certain actions (touching the walls, eating food, winning and dying), added chomp like motion to pacman and added AI movement - at this time, they are able to go through the containers

--- js/app.js
+++ js/app.js
@@ -16,10 +16,12 @@ class Board {
     this.position = position
     this.width = 60
     this.height = 60
+  
   }
   render = function() {
     ctx.fillStyle = 'blue'
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+
   }
 }
 
@@ -27,7 +29,8 @@ class Board {
 class Food {
   constructor({position}) {
     this.position = position
-    this.radius = 10
+    this.radius = 5
+
   }
   render = function() {
     ctx.beginPath()
@@ -72,7 +75,7 @@ gameBoard.forEach((row, index) => {
           position: {
             x: 60 * j,
             y: 60 * index
-          }
+          },
         })
       )
         break
@@ -96,11 +99,19 @@ class Player {
     this.position = position
     this.movement = movement
     this.radius = 20
+    this.rad = .75
+    this.mouth = .040
+    this.collectSound = new Audio('sounds/collectSound.wav')
+    this.deathSound = new Audio('sounds/deathSound.wav')
+    this.winSound = new Audio('sounds/winSound.wav')
+    this.bumpSound = new Audio('sounds/bump.wav')
   }
 
   render = function() {
+    ctx.save()
     ctx.beginPath()
-    ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+    ctx.arc(this.position.x, this.position.y, this.radius, this.rad, Math.PI * 2 - this.rad)
+    ctx.lineTo(this.position.x, this.position.y)
     ctx.fillStyle = 'yellow'
     ctx.fill()
     ctx.closePath()
@@ -109,6 +120,8 @@ class Player {
     this.render()
     this.position.x += this.movement.x
     this.position.y += this.movement.y
+    if (this.rad < 0 || this.rad > .75) this.mouth = -this.mouth
+    this.rad += this.mouth
   }
 
 }
@@ -126,8 +139,8 @@ const player = new Player({
 })
 
 //need to add moveset for our AI
-let dx = 2
-let dy = 2
+// let dx = 2
+// let dy = 2
 
 //add our enemies - same constructors as our player but will have different color 
 class Enemy {
@@ -143,16 +156,32 @@ class Enemy {
     ctx.fillStyle = 'red'
     ctx.fill()
     ctx.closePath()
-    this.position.x +=dx
-    this.position.y +=dy
+    // this.position.x = Math.random() * canvas.width
+    let diffX = player.position.x - this.position.x
+    let diffY = player.position.y - this.position.y
+    if (diffX > 0) {
+      this.position.x +=1
+    } else {
+      this.position.x -=1
+    }
+
+    if (diffY > 0) {
+      this.position.y +=1
+    } else {
+      this.position.y -=1
+    }
+
+
+
   }
   update = function() {
+    
     this.render()
     this.position.x += this.movement.x
     this.position.y += this.movement.y
   }
-}
 
+}
 
 const enemies = [
   new Enemy({
@@ -162,7 +191,7 @@ const enemies = [
     },
     movement: {
       x:0,
-      y:-5
+      y:0
     }
   })
 ]
@@ -170,8 +199,8 @@ const enemies = [
 const enemies2 = [
   new Enemy({
     position: {
-      x:980,
-      y:820
+      x:200,
+      y:800
     },
     movement: {
       x:0,
@@ -180,9 +209,10 @@ const enemies2 = [
   })
 ]
 
+
 // the gameloop function will basically allow the game to be a game (controlling what happens/when, movement/spawning items)
 const gameLoop = () => {
-  
+ 
   let gameAnimation = requestAnimationFrame(gameLoop)
   ctx.clearRect(0,0, canvas.width, canvas.height)
   tiles.forEach((tile) => {
@@ -193,7 +223,7 @@ const gameLoop = () => {
     && player.position.y + player.radius +player.movement.y>= tile.position.y 
     && player.position.x - player.radius + player.movement.x<= tile.position.x + tile.width) {
       // console.log('this should log if we are touching a wall')
-      
+      player.bumpSound.play()
       player.movement.x = 0
       player.movement.y =0
     }
@@ -201,32 +231,35 @@ const gameLoop = () => {
 })
 
 foods.forEach((food,index) => {
+  
   food.render()
 //remove food when we touch it 
   if (((food.position.x - player.position.x)*(food.position.x - player.position.x )) + (( food.position.y - player.position.y)*( food.position.y - player.position.y)) < 
   (food.radius + player.radius) * (food.radius + player.radius)) {
       foods.splice(index, 1)
-    
+      player.collectSound.play()
+      
       // console.log('this will notify us that we are touching the food')
       //change score whenever food is gone
       points +=5
       score.innerText = points
+     
       // win condition
       // console.log('should log win message when everything is collected', 'you win')
-      if (foods.length === 0) {
+       if (foods.length === 0) {
+         player.winSound.play()
         button.style.display === "none"
           button.style.display = "inline"
        cancelAnimationFrame(gameAnimation)
        alert(`You Won!, Final score of ${score.innerText}`)
       } 
       //populates button to play game again
-    
-      
     }
 })
 
 //spawn our player
-player.update()
+player.update() 
+  
 
 //spawns our enemies
 enemies.forEach(enemy=> {
@@ -240,30 +273,21 @@ enemies.forEach(enemy=> {
       && enemy.position.x - enemy.radius + enemy.movement.x<= tile.position.x + tile.width) {
         //  console.log('this should log if the AI touches a wall')
         enemy.movement.x = 0
-        enemy.movement.y =0
-
-        if( enemy.position.x  + dx > tile.width + enemy.radius ||  enemy.position.x  + dx < enemy.radius) {
-          dx = -dx;
-      }
-        if(enemy.position.y + dy > tile.height + enemy.radius || enemy.position.y + dy < enemy.radius) {
-          dy = -dy;
-      }
+        enemy.movement.y = 0
 
       }
   
-      
-      
-
   })
-
 
   if (((enemy.position.x - player.position.x)*(enemy.position.x - player.position.x )) + (( enemy.position.y - player.position.y)*( enemy.position.y - player.position.y)) < 
   (enemy.radius + player.radius) * (enemy.radius + player.radius)) {
-    // console.log('this should log a message when we touch an enemy','you lost')
+    // console.log('this should log a message when we touch an enemy','you lost'
     button.style.display === "none"
     button.style.display = "inline"
+    player.deathSound.play()
     cancelAnimationFrame(gameAnimation)
     alert(`Oh no, you died!, Final score of ${score.innerText}`)
+   
   }
 })
 
@@ -272,37 +296,39 @@ enemies2.forEach(enemy2=> {
   enemy2.render()
   //this will notify AI that they are touching a wall 
   tiles.forEach((tile) => {
+    
     tile.render()
     if (enemy2.position.y - enemy2.radius + enemy2.movement.y <= tile.position.y + tile.height 
       && enemy2.position.x + enemy2.radius + enemy2.movement.x >= tile.position.x 
       && enemy2.position.y + enemy2.radius +enemy2.movement.y>= tile.position.y 
       && enemy2.position.x - enemy2.radius + enemy2.movement.x<= tile.position.x + tile.width) {
-        // console.log('this should log if the AI touches a wall')
+        console.log('this should log if the AI touches a wall')
         enemy2.movement.x = 0
         enemy2.movement.y = 0
       }
-  
+
+
 
   })
-
-  
-
   if (((enemy2.position.x - player.position.x)*(enemy2.position.x - player.position.x )) + (( enemy2.position.y - player.position.y)*( enemy2.position.y - player.position.y)) < 
   (enemy2.radius + player.radius) * (enemy2.radius + player.radius)) {
     // console.log('this should log a message when we touch an enemy', 'you lost')
     button.style.display === "none"
     button.style.display = "inline"
+    player.deathSound.play()
     cancelAnimationFrame(gameAnimation)
+
     alert(`Oh no, you died!, Final score of ${score.innerText}`)
+
   }
 })
+
+
 }
-
-
-
-
+//end of game loop
 
 document.addEventListener('DOMContentLoaded', (e) => {
+  
   gameLoop()
   addEventListener('keydown', ({key}) => {
     // console.log('should log what key has been pressed via keyboard', key)
@@ -341,3 +367,4 @@ document.addEventListener('DOMContentLoaded', (e) => {
   })
   
 })
+
